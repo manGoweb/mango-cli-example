b77b7fd81922940eb5e6ed4157e829621c82622d
Jirka Vebr <jirkavebr@gmail.com>
2018-06-22 17:31:27 +0200

    Use TypeScript

---
+++ src/scripts/components/Emitter.ts
@@ -0,0 +1,44 @@
+import Component from './Component'
+
+type EventType = string
+type Events = Array<EventType>
+
+interface EmitterData {
+	events: EventType | Events
+}
+/**
+ * Emitter component class
+ *
+ * 	- emits given events on an element click
+ *
+ * Expects data = {
+ * 		events: [ 'event', 'names', 'to', 'trigger' ] - array of events or just single string
+ * }
+ */
+export default class Emitter extends Component<EmitterData> {
+
+	events: Events = []
+
+	initialize() {
+		this.events = Array.isArray(this.data.events) ? this.data.events : [this.data.events]
+	}
+
+	get listeners(): EventListeners {
+		return [
+			['click', this.handleClick]
+		]
+	}
+
+	handleClick(e: MouseEvent) {
+		e.preventDefault()
+
+		this.events.map(eventType => {
+			const event = document.createEvent('Event')
+
+			event.initEvent(eventType, true, true)
+
+			this.el.dispatchEvent(event)
+		})
+	}
+
+}
---
+++ src/scripts/index.ts
@@ -0,0 +1,13 @@
+import componentHandler from './componentHandler'
+
+import './plugins'
+
+import Example from './components/Example'
+import Shapes from './components/Shapes'
+
+
+// Sort the components alphabetically…
+componentHandler([
+	Example,
+	Shapes,
+])
