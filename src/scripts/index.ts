ce232ce0dcf2c2ba6de139dc06ab6f21fe08491f
Filip Chalupa <chalupa.filip@gmail.com>
2019-07-13 15:18:26 +0200

    In view upgrade

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,11 +3,13 @@ import { initializeComponents } from '@mangoweb/scripts-base'
 import './plugins'
 
 import Example from './components/Example'
+import { InView } from './components/InView'
 import Shapes from './components/Shapes'
 
 
 // Sort the components alphabetically…
 initializeComponents([
 	Example,
+	InView,
 	Shapes,
 ], 'initComponents')
